77ff92961a7c471b634576d25c8c12c20a8fbeae
dpetrov <daniel.petrov@tick42.com>
2017-06-28 15:59:12 +0300

    updated isImmutableCollection and RowUtils.isRowSelected to support immutable data

--- packages/react-data-grid/src/RowUtils.js
+++ packages/react-data-grid/src/RowUtils.js
@@ -1,3 +1,5 @@
+import { isImmutableCollection } from './utils'
+
 let RowUtils = {
   get: function(row, property) {
     if (typeof row.get === 'function') {
@@ -10,8 +12,14 @@ let RowUtils = {
     if (indexes && Object.prototype.toString.call(indexes) === '[object Array]') {
       return indexes.indexOf(rowIdx) > -1;
     } else if (keys && keys.rowKey && keys.values && Object.prototype.toString.call(keys.values) === '[object Array]') {
+      if (isImmutableCollection(rowData)) {
+        return keys.values.indexOf(rowData.get(keys.rowKey)) > -1;
+      }
       return keys.values.indexOf(rowData[keys.rowKey]) > -1;
     } else if (isSelectedKey && rowData && typeof isSelectedKey === 'string') {
+      if (isImmutableCollection(rowData)) {
+        return rowData.get(isSelectedKey);
+      }
       return rowData[isSelectedKey];
     }
     return false;
